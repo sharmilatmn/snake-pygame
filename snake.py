ee78820c9efbd59826832e626b7a344e7de8bb95
d03963st <sharmila.thirumurugan@student.manchester.ac.uk>
2022-08-12 12:06:43 +0100

    created food with randomly generated positions

--- snake.py
+++ snake.py
@@ -1,8 +1,23 @@
-import pygame, sys
+import pygame, sys, random
+from pygame.math import Vector2
+
+class Food:
+    def __init__(self):
+        self.x = random.randint(0, cell_number - 1) # -1 ensures food always on-screen
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y) # creating 2D vector to determine position
+
+    def draw_food(self):
+        food_rect = pygame.Rect(int(self.pos.x) * cell_size, int(self.pos.y) * cell_size, cell_size, cell_size) # creates a rectangle: (xpos, ypos, h, w)
+        pygame.draw.rect(screen, (66, 161, 255), food_rect)
 
 pygame.init() # initialising pygame modules
-screen = pygame.display.set_mode((400, 500)) # creating base surface (like a blank canvas)
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size)) # creating base surface (like a blank canvas)
 clock = pygame.time.Clock() # limits how fast while loop can run
+
+food = Food()
 
 while True:
     for event in pygame.event.get():
@@ -11,5 +26,6 @@ while True:
             sys.exit() # ensures all parts of prog have stopped running
 
     screen.fill((230, 242, 233)) 
+    food.draw_food()
     pygame.display.update() # ensures elements show up on screen
     clock.tick(60) # sets framerate: how many times the while loop can run per sec
